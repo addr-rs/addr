49673ce797ca3155e3ce58e1829664b729dfc994
rushmorem <rushmore@webenchanter.com>
2018-07-08 16:06:29 +0200

    bump Travis timeout to 30 minutes

--- benches/list_benchmark.rs
+++ benches/list_benchmark.rs
@@ -10,16 +10,16 @@ use criterion::Criterion;
 fn criterion_benchmark(c: &mut Criterion) {
     let list = List::new();
 
-    c.bench_function(".com domain", move |b| {
-        b.iter(|| { list.registrable_domain(&"eXample.cOm".to_lowercase()).unwrap(); } )
+    c.bench_function("first rule", move |b| {
+        b.iter(|| { list.registrable_domain(&"eXample.gb.com".to_lowercase()).unwrap(); } )
     });
 
-    c.bench_function(".co.uk domain", move |b| {
-        b.iter(|| { list.registrable_domain(&"eXample.cO.uk".to_lowercase()).unwrap(); } )
+    c.bench_function("deeply nested", move |b| {
+        b.iter(|| { list.registrable_domain(&"eXample.dyndns-server.com".to_lowercase()).unwrap(); } )
     });
 
-    c.bench_function(".co.zw domain", move |b| {
-        b.iter(|| { list.registrable_domain(&"eXample.cO.zw".to_lowercase()).unwrap(); } )
+    c.bench_function("popular rule", move |b| {
+        b.iter(|| { list.registrable_domain(&"eXample.com".to_lowercase()).unwrap(); } )
     });
 }
 
