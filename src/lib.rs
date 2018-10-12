f24f00269d22625c3c9e6b4eab4531e8ff6a06d3
rushmorem <rushmore@webenchanter.com>
2018-10-12 12:46:23 +0200

    make doc comment compatible with Rust v1.25.0

--- src/lib.rs
+++ src/lib.rs
@@ -11,37 +11,36 @@
   use addr::{DomainName, DnsName, Email};
   # use addr::Result;
 
-  # fn main() -> Result<()> {
+  # fn main() {
     // You can find out the root domain
     // or extension of any given domain name
     let domain: DomainName = "www.example.com".parse().unwrap();
     assert_eq!(domain.root(), "example.com");
     assert_eq!(domain.root().suffix(), "com");
 
-    let domain: DomainName = "www.食狮.中国".parse()?;
+    let domain: DomainName = "www.食狮.中国".parse().unwrap();
     assert_eq!(domain.root(), "xn--85x722f.xn--fiqs8s");
     assert_eq!(domain.root().suffix(), "xn--fiqs8s");
 
-    let domain: DomainName = "www.xn--85x722f.xn--55qx5d.cn".parse()?;
+    let domain: DomainName = "www.xn--85x722f.xn--55qx5d.cn".parse().unwrap();
     assert_eq!(domain.root(), "xn--85x722f.xn--55qx5d.cn");
     assert_eq!(domain.root().suffix(), "xn--55qx5d.cn");
 
-    let domain: DomainName = "a.b.example.uk.com".parse()?;
+    let domain: DomainName = "a.b.example.uk.com".parse().unwrap();
     assert_eq!(domain.root(), "example.uk.com");
     assert_eq!(domain.root().suffix(), "uk.com");
 
-    let name: DnsName = "_tcp.example.com.".parse()?;
+    let name: DnsName = "_tcp.example.com.".parse().unwrap();
     assert_eq!(name.root(), "example.com.");
     assert_eq!(name.root().suffix(), "com.");
 
-    let email: Email = "чебурашка@ящик-с-апельсинами.рф".parse()?;
+    let email: Email = "чебурашка@ящик-с-апельсинами.рф".parse().unwrap();
     assert_eq!(email.user(), "чебурашка");
     assert_eq!(email.host(), "xn-----8kcayoeblonkwzf2jqc1b.xn--p1ai");
 
     // In any case if the domain's suffix is in the list
     // then this is definately a registrable domain name
     assert!(domain.root().suffix().is_known());
-  # Ok(())
   # }
   ```
 !*/
