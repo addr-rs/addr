57a43c59ad9c827e8a143ea72a01ea3f8a787a6f
rushmorem <rushmore@webenchanter.com>
2021-03-02 02:21:13 +0200

    update benchmarks

--- benches/list_benchmark.rs
+++ benches/list_benchmark.rs
@@ -1,18 +1,17 @@
+use addr::DomainName;
 use criterion::{criterion_group, criterion_main, Criterion};
 use psl::{List, Psl};
 
 fn psl(c: &mut Criterion) {
-    let list = List::new();
-
-    c.bench_function("public suffix", move |b| {
+    c.bench_function("psl", |b| {
         b.iter(|| {
-            list.suffix("example.com").unwrap();
+            List.domain("example.com").unwrap();
         })
     });
 
-    c.bench_function("registrable domain", move |b| {
+    c.bench_function("addr", |b| {
         b.iter(|| {
-            list.domain("example.com").unwrap();
+            "example.com".parse::<DomainName>().unwrap();
         })
     });
 }
