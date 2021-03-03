9231f65f2f20db78ffede322bff7d797a702bd77
rushmorem <rushmore@webenchanter.com>
2021-03-03 02:23:40 +0200

    optimise the parser

--- src/parser.rs
+++ src/parser.rs
@@ -4,16 +4,6 @@ static MAX_DOMAIN_LEN: usize = 253;
 static MAX_LABELS_COUNT: usize = 127;
 static MAX_LABEL_LEN: usize = 63;
 
-#[inline]
-fn is_tld(input: &str) -> bool {
-    !gt_max_label_len(input) && input.parse::<f64>().is_err()
-}
-
-#[inline]
-fn gt_max_label_len(label: &str) -> bool {
-    label.len() > MAX_LABEL_LEN
-}
-
 /// Check if a domain has valid syntax
 // https://en.wikipedia.org/wiki/Domain_name#Domain_name_syntax
 // http://blog.sacaluta.com/2011/12/dns-domain-names-253-or-255-bytesoctets.html
@@ -22,62 +12,47 @@ fn gt_max_label_len(label: &str) -> bool {
 pub fn parse_domain(punycode: &str) -> Result<()> {
     if !punycode.is_ascii() {
         return Err(Error::NotAscii);
-    };
-    let is_valid = {
-        let punycode = if punycode.ends_with('.') {
-            &punycode[..punycode.len() - 1]
-        } else {
-            &punycode
-        };
-        let labels = punycode.rsplit('.');
-        // check total lengths
-        if punycode.len() > MAX_DOMAIN_LEN || labels.clone().count() > MAX_LABELS_COUNT {
-            false
-        } else {
-            let first_is_tld = labels.clone().next().map(is_tld);
-            // check individual labels
-            if first_is_tld == Some(false)
-                || first_is_tld.is_none()
-                || labels.clone().any(gt_max_label_len)
-            {
-                false
-            } else {
-                let check_labels = || {
-                    for label in labels {
-                        if label.trim().is_empty() {
-                            return false;
-                        }
-                        let chars = label.chars();
-                        let last = label.len() - 1;
-                        for (i, c) in chars.enumerate() {
-                            if ((i == 0 || i == last) && !c.is_alphanumeric())
-                                || (c != '-' && !c.is_alphanumeric())
-                            {
-                                return false;
-                            }
-                        }
-                    }
-                    true
-                };
-                check_labels()
-            }
-        }
-    };
-    if is_valid {
-        Ok(())
-    } else {
-        Err(Error::InvalidDomain)
     }
+
+    let domain = punycode.strip_suffix('.').unwrap_or(punycode);
+
+    // check total lengths
+    if domain.len() > MAX_DOMAIN_LEN {
+        return Err(Error::InvalidDomain);
+    }
+
+    let dot_count = domain.matches('.').count();
+
+    if dot_count + 1 > MAX_LABELS_COUNT {
+        return Err(Error::InvalidDomain);
+    }
+
+    for (i, label) in domain.split('.').enumerate() {
+        let len = label.len();
+
+        if label.trim().is_empty() || len > MAX_LABEL_LEN {
+            return Err(Error::InvalidDomain);
+        }
+
+        if i == dot_count && label.parse::<f64>().is_ok() {
+            return Err(Error::InvalidDomain);
+        }
+
+        if !label.starts_with(char::is_alphanumeric) || !label.ends_with(char::is_alphanumeric) {
+            return Err(Error::InvalidDomain);
+        }
+
+        if label.contains(|c: char| c != '-' && !c.is_alphanumeric()) {
+            return Err(Error::InvalidDomain);
+        }
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn invalid_tld() {
-        assert!(!is_tld("1234"));
-    }
 
     #[test]
     fn single_label_domain() {
