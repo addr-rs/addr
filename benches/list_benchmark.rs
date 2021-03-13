c5beed4a2d8cc34f3fca476beaded68831fb242e
rushmorem <rushmore@webenchanter.com>
2021-03-13 19:21:04 +0200

    update benchmarks

--- benches/list_benchmark.rs
+++ benches/list_benchmark.rs
@@ -1,34 +1,46 @@
-use addr::{dns, domain, email};
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn psl(c: &mut Criterion) {
     c.bench_function("psl::suffix", |b| {
+        use psl::{Psl, List};
+
         b.iter(|| {
-            psl::suffix(b"example.com").unwrap();
+            List.suffix(b"example.com").unwrap();
         })
     });
 
     c.bench_function("psl::domain", |b| {
+        use psl::{Psl, List};
+
         b.iter(|| {
-            psl::domain(b"example.com").unwrap();
+            List.domain(b"example.com").unwrap();
         })
     });
 
-    c.bench_function("addr::domain::Name::parse", |b| {
+    c.bench_function("addr::parser::DomainName", |b| {
+        use addr::parser::DomainName;
+        use psl::List;
+
         b.iter(|| {
-            domain::Name::parse("example.com").unwrap();
+            List.parse_domain_name("example.com").unwrap();
         })
     });
 
-    c.bench_function("addr::dns::Name::parse", |b| {
+    c.bench_function("addr::parser::DnsName", |b| {
+        use addr::parser::DnsName;
+        use psl::List;
+
         b.iter(|| {
-            dns::Name::parse("_example.com").unwrap();
+            List.parse_dns_name("_example.com").unwrap();
         })
     });
 
     c.bench_function("addr::email::Address::parse", |b| {
+        use addr::parser::EmailAddress;
+        use psl::List;
+
         b.iter(|| {
-            email::Address::parse("john.doe@example.com").unwrap();
+            List.parse_email_address("john.doe@example.com").unwrap();
         })
     });
 }
